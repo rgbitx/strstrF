f712004c2bfd0e3649538c5b4fd334b597680b85
rgbitx <rgbitx@gmail.com>
2016-03-29 22:10:32 +0900

    t1

--- main.cpp
+++ main.cpp
@@ -2,32 +2,34 @@
 
 using namespace std;
 
-int strstr(string haystack, string needle){
+int strstr(string haystack, string needle) {
 
-    for(int i=0;;i++){
-        for(int j=0;;j++){
+    for (int i = 0; ; i++) {
+        for (int j = 0; ; j++) {
             // if j equals to needle's length
-            if(j==needle.size()) return i;
-            if(i+j==haystack.size()) return -1;
-            if(needle[j]!=haystack[i+j]) break;
+            if (j == needle.size()) return i;
+
+            //if i + j are out of the haystack size, no need to compare
+            if (i + j == haystack.size()) return -1;
+
+            //to compare each char
+            if (needle[j] != haystack[i + j]) break;
         }
     }
 
 }
 
 
-
 int main() {
     cout << "Hello, World!" << endl;
 
-    string needle="acd";
-    string haystack="adfewracdadfasdfasdf";
+    // test strings
+    string needle = "acd";
+    string haystack = "adfewracdadfasdfasdf";
 
-    int ret=strstr(haystack,needle);
+    int ret = strstr(haystack, needle);
 
-    cout<<ret<<endl;
-
-
+    cout << ret << endl;
 
     return 0;
 }
